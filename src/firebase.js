31d40249b20bc39a56ebc2ec782e0a2433bf09f2
envision3dorders-afk <envision3dorders@gmail.com>
2026-05-18 11:16:10 +0200

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwFA-qkuGpXddzL5CY5ItNJR_XZ25ho1w",
+  authDomain: "envision3d-orders.firebaseapp.com",
+  projectId: "envision3d-orders",
+  storageBucket: "envision3d-orders.firebasestorage.app",
+  messagingSenderId: "434059997461",
+  appId: "1:434059997461:web:cb4fb402e2df00a0bc791c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
